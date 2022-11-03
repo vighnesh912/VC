e3424b5e67aec5eed2d466c593661586bca68c32
vigne <vigneshmane@gmail.com>
2022-11-03 23:07:35 +0000

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Vighnesh ")
+    sport = input("Basketball")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
